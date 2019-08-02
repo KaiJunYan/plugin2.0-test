f412630287d6895414b373fd816ec8fa63bebaee
kaijun <kaijun@fit2cloud.com>
2019-08-02 17:26:36 +0800

    update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@ cd /opt/fit2cloud-plugin2.0-test
 echo going to start app
 nohup java -jar plugin-test-1.0.0.jar > log.txt 2>&1 &
 echo app started!
-ps aux | grep SimpleHTTPServer | grep -v grep
+ps aux | grep java | grep -v grep
